6ce1375ebc3dbe4a91b73d698e443522c4928c85
Carlo Clamucha <carlo.clamucha@gmail.com>
2019-02-13 17:18:36 -0600

    yess

--- taco-log/backend/app.js
+++ taco-log/backend/app.js
@@ -1,14 +1,23 @@
 const http = require('http');
+const server = require('express')();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+server.get('/', (req, res) => {
+  res.json({ message: 'yay im running' });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(process.env.PORT, () => {
+  console.log('yess');
 });
